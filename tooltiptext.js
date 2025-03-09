1e6dab9195298e9a1d534dcb9ea745f2d9650531
Fanambinantsoa Miavaka Nofidiana <30421229+miavakanofidiana@users.noreply.github.com>
2025-03-09 14:29:07 +0300

    init

---
+++ tooltiptext.js
@@ -0,0 +1,8 @@
+const multiplicateurTooltipText = "<b>Multiplicateur</b><br>Il augmente la vitesse de l'<b>Arbre lent</b> de 1000 à 2000 tours/minute.<br><img class='leg' src='images/multiplicateur.jpg'><br><i>Source: </i>";
+const matTooltipText = "<b>Mât</b><br>Le mât élève l'éolienne pour capter des vents plus puissants en altitude d’environ 85 à 110 mètres de haut.<br><img class='leg' src='images/parc_eolien.jpg'><br><i>Source: </i>";
+const transformateurTooltipText = "<b>Transformateur</b><br>Ceci tranforme le courant électrique en haute tension jusqu'à 30 000 V pour être injecté sur le réseau électrique.<br><img class='leg' src='images/transformateur.jpg'><br><i>Source: </i>";
+const palesTooltipText = "<b>Pales</b><br>Les pales convertissent l'<em>énergie cinétique</em> du vent en <em>énergie mécanique.</em>Celles-ci tournent à une vitesse d’environ 10 à 25 tours/minute.";
+const alternateurTooltipText = "<b>Alternateur</b><br>L'alternateur convertit l'<em>énergie mécanique</em> du multiplicateur en <em>énergie électrique.</em> Il fonctionne sur le principe de la dynamo.<br><img class='leg' src='images/DYNAMO-ALTERNATEUR PRINCIPE ANIME 1.gif'><br><i>Source: </i>";
+const windTooltipText = "<br>Particules d'air.";
+const arbreLentTooltipText = "<br>Arbre lent.";
+const pylonTooltipText = "Pylône.<br><img class='leg' src='images/pylone.jpg'><br><i>Source: </i>";
